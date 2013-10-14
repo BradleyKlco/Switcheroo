ee28571143a6a2beecb73f9f1294e8658d4562c7
Bradley <bradleyklco@gmail.com>
2013-10-14 13:30:08 -0500

    Added non-responsive option

--- js/products.js
+++ js/products.js
@@ -11,7 +11,8 @@ $products = {
 		img      : 'http://i.imgur.com/hgp2v0H.jpg',
 		url      : 'http://visia.themes.pixelentity.com/',
 		purchase : 'http://themeforest.net/item/visia-responsive-one-page-retina-wordpress-theme/5602067?ref=OriginalEXE',
-		tooltip  : 'Optional Tooltip'
+		tooltip  : 'Optional Tooltip',
+		responsive  : true,
 	},
 
 	bigwig_wp : {
@@ -19,7 +20,8 @@ $products = {
 		tag      : 'WP',
 		img      : 'http://i.imgur.com/uoreaON.jpg',
 		url      : 'http://bigwig.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bigwig-modern-corporate-retina-wordpress-theme/5217458?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/bigwig-modern-corporate-retina-wordpress-theme/5217458?ref=OriginalEXE',
+		responsive  : false,
 	},
 
 	surreal : {
@@ -27,7 +29,8 @@ $products = {
 		tag      : 'WP',
 		img      : 'http://i.imgur.com/yISKP2n.jpg',
 		url      : 'http://surreal.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/surreal-one-page-parallax-wordpress-theme/5175040?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/surreal-one-page-parallax-wordpress-theme/5175040?ref=OriginalEXE',
+		responsive  : true,
 	},
 
 	halfcreative : {
@@ -35,7 +38,8 @@ $products = {
 		tag      : 'WP',
 		img      : 'http://i.imgur.com/roUmrA5.jpg',
 		url      : 'http://halfcreative.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/halfcreative-one-page-portfolio-wordpress-theme/4566909?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/halfcreative-one-page-portfolio-wordpress-theme/4566909?ref=OriginalEXE',
+		responsive  : false,
 	},
 
 	bigwig_html : {
@@ -43,7 +47,8 @@ $products = {
 		tag      : 'HTML',
 		img      : 'http://i.imgur.com/rOk3oel.jpg',
 		url      : 'http://bigwig.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bigwig-modern-corporate-html5-template/4801306?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/bigwig-modern-corporate-html5-template/4801306?ref=OriginalEXE',
+		responsive  : true,
 	},
 
 	hydra_wp : {
@@ -51,7 +56,8 @@ $products = {
 		tag      : 'WP',
 		img      : 'http://i.imgur.com/RR11Nw9.jpg',
 		url      : 'http://hydra.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-wordpress-theme/4352507?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-wordpress-theme/4352507?ref=OriginalEXE',
+		responsive  : false,
 	},
 
 	contour : {
@@ -59,7 +65,8 @@ $products = {
 		tag      : 'HTML',
 		img      : 'http://i.imgur.com/5RntBHM.jpg',
 		url      : 'http://contour.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/contour-minimal-portfolio-corporate-template/4750249?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/contour-minimal-portfolio-corporate-template/4750249?ref=OriginalEXE',
+		responsive  : true,
 	},
 
 	brandspace : {
@@ -67,7 +74,8 @@ $products = {
 		tag      : 'WP',
 		img      : 'http://i.imgur.com/9o0TRje.jpg',
 		url      : 'http://brandspace.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/brandspace-responsive-portfolio-business-theme/3502985?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/brandspace-responsive-portfolio-business-theme/3502985?ref=OriginalEXE',
+		responsive  : false,
 	},
 
 	bon_atelier : {
@@ -75,7 +83,8 @@ $products = {
 		tag      : 'HTML',
 		img      : 'http://i.imgur.com/9o0TRje.jpg',
 		url      : 'http://brandspace.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bon-atelier-responsive-one-page-html5-template/3993470?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/bon-atelier-responsive-one-page-html5-template/3993470?ref=OriginalEXE',
+		responsive  : true,
 	},
 
 	mentor_html : {
@@ -83,7 +92,8 @@ $products = {
 		tag      : 'HTML',
 		img      : 'http://i.imgur.com/f0g6JU5.jpg',
 		url      : 'http://mentor.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/mentor-premium-responsive-business-html5-template/2374421?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/mentor-premium-responsive-business-html5-template/2374421?ref=OriginalEXE',
+		responsive  : false,
 	},
 
 	mentor_wp : {
@@ -91,7 +101,8 @@ $products = {
 		tag      : 'WP',
 		img      : 'http://i.imgur.com/8CSwoGS.jpg',
 		url      : 'http://mentor.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/mentor-premium-responsive-html5-wordpress-theme/2560147?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/mentor-premium-responsive-html5-wordpress-theme/2560147?ref=OriginalEXE',
+		responsive  : true,
 	},
 
 	hydra_html : {
@@ -99,7 +110,8 @@ $products = {
 		tag      : 'HTML',
 		img      : 'http://i.imgur.com/qIvg1HU.jpg',
 		url      : 'http://hydra.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-html5-template/4158593?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-html5-template/4158593?ref=OriginalEXE',
+		responsive  : false,
 	},
 
 	neo_wp : {
@@ -107,7 +119,8 @@ $products = {
 		tag      : 'WP',
 		img      : 'http://i.imgur.com/BXbOoUp.jpg',
 		url      : 'http://neo.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/neo-premium-responsive-business-wordpress-theme/2916870?ref=OriginalEXE'
+		purchase : 'http://themeforest.net/item/neo-premium-responsive-business-wordpress-theme/2916870?ref=OriginalEXE',
+		responsive  : true,
 	}
 
 };
